ee3c93b457b767e895ec97869e2ae6fd710754a4
Rugwed Pimple <rugwedpimpale@gmail.com>
2024-01-28 16:25:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,57 +1,57 @@
-#Dependencies 
-import os
-from apkikey import apikey
+# Bring in deps
+import os 
+from apikey import apikey 
 
-import streamlit as st
-from langchain.llms import openai
+import streamlit as st 
+from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain, SequentialChain
+from langchain.chains import LLMChain, SequentialChain 
 from langchain.memory import ConversationBufferMemory
-from langchain.utilities import wikipedia
+from langchain.utilities import WikipediaAPIWrapper 
 
 os.environ['OPENAI_API_KEY'] = apikey
 
-#App framework 
-st.title('🦜🔗Script GPT Creator')
-prompt = st.text_input('Plug in your prompt here')
+# App framework
+st.title('🦜🔗 YouTube GPT Creator')
+prompt = st.text_input('Plug in your prompt here') 
 
-#prompt template
+# Prompt templates
 title_template = PromptTemplate(
-    input_variable = ['topic'],
-    template = 'Write me a youtube video title about {topic}'
+    input_variables = ['topic'], 
+    template='write me a youtube video title about {topic}'
 )
 
-scipt_template = PromptTemplate(
-    input_variable = ['title', 'wikipedia_research'],
-    template = 'Write me a youtube video script vased on this title TITLE: {title} While leveraging this wikipedia research:{wikipedia_research}'
+script_template = PromptTemplate(
+    input_variables = ['title', 'wikipedia_research'], 
+    template='write me a youtube video script based on this title TITLE: {title} while leveraging this wikipedia reserch:{wikipedia_research} '
 )
 
-#Memory
-
-title_memory = ConversationBufferMemory(input_key='topic', memory_key='chat_history') 
+# Memory 
+title_memory = ConversationBufferMemory(input_key='topic', memory_key='chat_history')
 script_memory = ConversationBufferMemory(input_key='title', memory_key='chat_history')
 
-#llms
-llm = openai(temperature=0.9)
+
+# Llms
+llm = OpenAI(temperature=0.9) 
 title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True, output_key='title', memory=title_memory)
-script_chain = LLMChain(llm=llm, prompt=scipt_template, verbose=True, output_key='script', memory= script_memory)
-#sequential_chain = SequentialChain(chains=[title_chain, script_chain], input_variables=['topic'], output_variables= ['title', 'script'], verbose=True)
-wiki = wikipedia()
+script_chain = LLMChain(llm=llm, prompt=script_template, verbose=True, output_key='script', memory=script_memory)
 
-#Display on screen
-if prompt:
+wiki = WikipediaAPIWrapper()
+
+# Show stuff to the screen if there's a prompt
+if prompt: 
     title = title_chain.run(prompt)
-    wiki_research = wiki.run(prompt)
-    script = script_chain.run(title=title, wiki_research = wiki_research)
-   
-    st.write(title)
-    st.write(script)
+    wiki_research = wiki.run(prompt) 
+    script = script_chain.run(title=title, wikipedia_research=wiki_research)
 
-    with st.expander('Title History'):
+    st.write(title) 
+    st.write(script) 
+
+    with st.expander('Title History'): 
         st.info(title_memory.buffer)
 
-    with st.expander('Script History'):
+    with st.expander('Script History'): 
         st.info(script_memory.buffer)
 
-    with st.expander('Wikipedia Research History'):
-        st.info(wiki_research)    
+    with st.expander('Wikipedia Research'): 
+        st.info(wiki_research)
