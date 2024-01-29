51507c2535e7bb62c5a56b54b47ef1acfb0ebd82
Rugwed Pimple <rugwedpimpale@gmail.com>
2024-01-29 00:36:50 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,57 +1,49 @@
-# Bring in deps
-import os 
-from apikey import apikey 
-
-import streamlit as st 
-from langchain.llms import OpenAI
+# Dependencies
+import os
+import streamlit as st
+from langchain_community.llms import OpenAI
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain, SequentialChain 
+from langchain.chains import LLMChain
 from langchain.memory import ConversationBufferMemory
-from langchain.utilities import WikipediaAPIWrapper 
 
+# Set your OpenAI API Key
+apikey = "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"  # Replace with your actual API key
 os.environ['OPENAI_API_KEY'] = apikey
 
 # App framework
-st.title('🦜🔗 Script Generator GPT')
-prompt = st.text_input('Plug in your prompt here') 
+st.title('🦜🔗 Script GPT Creator')
+prompt = st.text_input('Plug in your prompt here')
 
 # Prompt templates
 title_template = PromptTemplate(
-    input_variables = ['topic'], 
-    template='write me a youtube video title about {topic}'
+    input_variables=['topic'],
+    template='Write me a YouTube video title about {topic}'
 )
 
 script_template = PromptTemplate(
-    input_variables = ['title', 'wikipedia_research'], 
-    template='write me a youtube video script based on this title TITLE: {title} while leveraging this wikipedia reserch:{wikipedia_research} '
+    input_variables=['title'],
+    template='Write me a YouTube video script based on this title TITLE: {title}'
 )
 
-# Memory 
+# Memory
 title_memory = ConversationBufferMemory(input_key='topic', memory_key='chat_history')
 script_memory = ConversationBufferMemory(input_key='title', memory_key='chat_history')
 
-
-# Llms
-llm = OpenAI(temperature=0.9) 
+# LLMs
+llm = OpenAI(temperature=0.9)
 title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True, output_key='title', memory=title_memory)
 script_chain = LLMChain(llm=llm, prompt=script_template, verbose=True, output_key='script', memory=script_memory)
 
-wiki = WikipediaAPIWrapper()
-
-# Show stuff to the screen if there's a prompt
-if prompt: 
+# Display on screen
+if prompt:
     title = title_chain.run(prompt)
-    wiki_research = wiki.run(prompt) 
-    script = script_chain.run(title=title, wikipedia_research=wiki_research)
+    script = script_chain.run(title=title)
 
-    st.write(title) 
-    st.write(script) 
+    st.write(title)
+    st.write(script)
 
-    with st.expander('Title History'): 
+    with st.expander('Title History'):
         st.info(title_memory.buffer)
 
-    with st.expander('Script History'): 
+    with st.expander('Script History'):
         st.info(script_memory.buffer)
-
-    with st.expander('Wikipedia Research'): 
-        st.info(wiki_research)
